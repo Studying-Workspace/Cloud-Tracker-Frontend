758426d203de39cf9c79249ac20c16595dd80896
1Ahmedzedan <ugs.141182@ci.suez.edu.eg>
2024-03-16 05:56:14 +0200

    Add Navigator in blog page

--- src/ui/Blog/BlogNavigator.tsx
+++ src/ui/Blog/BlogNavigator.tsx
@@ -1,7 +1,58 @@
-const BlogNavigator = () => {
-  return (
-    <div>BlogNavigator</div>
-  )
-}
+import { Tab, Tabs, tabsClasses } from "@mui/material";
 
-export default BlogNavigator
+interface Props {
+  value : number ; 
+  setValue : Function ; 
+};
+
+const BlogNavigator = ({value , setValue}:Props) => {
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className="px-24">
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        variant="scrollable"
+        scrollButtons
+        allowScrollButtonsMobile
+        aria-label="visible arrows tabs example"
+        sx={{
+          "& .MuiTabs-scroller": {
+            "& .MuiTabs-flexContainer": {
+              justifyContent: "space-between", 
+              //borderBottom: "solid 1px gray"
+            },
+          },
+          [`& .${tabsClasses.scrollButtons}`]: {
+            "&.Mui-disabled": { opacity: 0.3 },
+          },
+          "& .MuiTab-root": {
+            //minWidth: "auto", 
+            width: "calc(100%/4)", 
+            fontSize: "16px",
+            textTransform: "capitalize",
+          },
+          "& .Mui-selected": {
+            color: "black !important",
+            fontSize: "16px",
+            fontWeight: "bold",
+            background: "rgba(0,0,0,10%)",
+          },
+          "& .MuiTabs-indicator": {
+            display: "none !important", // Hide the active indicator
+          },
+        }}
+      >
+        <Tab label="All" />
+        <Tab label="Machine Learning" />
+        <Tab label="Web Development" />
+        <Tab label="Technology" />
+      </Tabs>
+    </div>
+  );
+};
+
+export default BlogNavigator;
--- src/ui/Blog/BlogsCotainer.tsx
+++ src/ui/Blog/BlogsCotainer.tsx
@@ -1,19 +1,28 @@
-import BlogElement from "./BlogElement"
+import BlogElement from "./BlogElement";
+import { useState } from "react";
+import BlogNavigator from "./BlogNavigator";
 
 const BlogsCotainer = () => {
-  return (
-    <div className="px-24 h-full overflow-auto divide-y-2">
-        <BlogElement/>
-        <BlogElement/>
-        <BlogElement/>
-        <BlogElement/>
-        <BlogElement/>
-        <BlogElement/>
-        <BlogElement/>
-        <BlogElement/>
-        <BlogElement/>
-    </div>
-  )
-}
+  const [value, setValue] = useState<number>(0);
 
-export default BlogsCotainer
+  
+
+  return (
+    <div className="h-full divide-y-2 divide-linearOrange-100 py-12">
+      <BlogNavigator value={value} setValue={setValue}/>
+      <div className="h-full divide-y-2 overflow-auto px-24">
+        <BlogElement />
+        <BlogElement />
+        <BlogElement />
+        <BlogElement />
+        <BlogElement />
+        <BlogElement />
+        <BlogElement />
+        <BlogElement />
+        <BlogElement />
+      </div>
+    </div>
+  );
+};
+
+export default BlogsCotainer;
