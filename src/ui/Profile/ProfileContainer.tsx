c30ffec9901160e2b0dd4a3397a4a74a1bd320bf
Noran Sherif <noranosherif958@gmail.com>
2024-04-01 21:46:09 +0200

    *Add input fields to profile page

--- src/ui/Profile/ProfileContainer.tsx
+++ src/ui/Profile/ProfileContainer.tsx
@@ -1,11 +1,12 @@
 import { IoCloudUploadOutline } from "react-icons/io5";
 import Button from "../Button";
+import { MdEdit } from "react-icons/md";
 
 const ProfileContainer = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center">
+    <div className="flex min-h-screen items-center justify-center ">
       <div className="bg-gradient-to-br from-linearBlue-2 to-linearOrange-100 p-[4px]">
-        <div className="flex h-[500px] w-[900px] items-center justify-start bg-white px-[100px]">
+        <div className=" flex h-[450px] w-[1000px] content-between items-center justify-start gap-16 bg-white px-[100px]">
           {/* Profile Image */}
           <div className="relative flex flex-col items-center gap-8">
             <input
@@ -26,6 +27,61 @@ const ProfileContainer = () => {
                 Upload Photo{" "}
               </Button>
             </label>
+          </div>
+
+          {/*input fields*/}
+          <div className="flex flex-wrap gap-[36px]">
+            <div className=" bg-gradient-to-br from-linearBlue-2 to-linearOrange-100 pb-[1.5px]">
+              <div className="flex h-[35px] w-[250px] justify-between bg-white ">
+                <input
+                  type="text"
+                  className="w-full px-2 text-lg text-gray-600 focus:outline-none"
+                  placeholder="Username"
+                />{" "}
+                <span className="text-2xl text-gray-500">
+                  <MdEdit />
+                </span>
+              </div>
+            </div>
+
+            <div className=" bg-gradient-to-br from-linearBlue-2 to-linearOrange-100 pb-[1.5px]">
+              <div className="flex h-[35px] w-[250px] justify-between bg-white ">
+                <input
+                  type="text"
+                  className="w-full px-2 text-lg text-gray-600 focus:outline-none"
+                  placeholder="Password"
+                />{" "}
+                <span className="text-2xl text-gray-500">
+                  <MdEdit />
+                </span>
+              </div>
+            </div>
+
+            <div className=" bg-gradient-to-br from-linearBlue-2 to-linearOrange-100 pb-[1.5px]">
+              <div className="flex h-[35px] w-[250px] justify-between bg-white ">
+                <input
+                  type="text"
+                  className="w-full px-2 text-lg text-gray-600 focus:outline-none"
+                  placeholder="Email"
+                />{" "}
+                <span className="text-2xl text-gray-500">
+                  <MdEdit />
+                </span>
+              </div>
+            </div>
+
+            <div className=" bg-gradient-to-br from-linearBlue-2 to-linearOrange-100 pb-[1.5px]">
+              <div className="flex h-[35px] w-[250px] justify-between bg-white ">
+                <input
+                  type="text"
+                  className="w-full px-2 text-lg text-gray-600 focus:outline-none"
+                  placeholder="Country"
+                />{" "}
+                <span className="text-2xl text-gray-500">
+                  <MdEdit />
+                </span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
