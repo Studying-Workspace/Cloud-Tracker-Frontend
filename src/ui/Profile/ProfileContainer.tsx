12256cf54eea02a97620442b3b8ba7883d5c5f2e
Noran Sherif <noranosherif958@gmail.com>
2024-04-01 17:35:03 +0200

    * Fix upload photo button
    - Add absolute to input field so the button uploads photo

--- src/ui/Profile/ProfileContainer.tsx
+++ src/ui/Profile/ProfileContainer.tsx
@@ -7,8 +7,12 @@ const ProfileContainer = () => {
       <div className="bg-gradient-to-br from-linearBlue-2 to-linearOrange-100 p-[4px]">
         <div className="flex h-[500px] w-[1200px] items-center justify-start bg-white px-16">
           {/* Profile Image */}
-          <div className="flex flex-col items-center gap-8">
-            <input type="file" id="file-upload" className="hidden" />
+          <div className="relative flex flex-col items-center gap-8">
+            <input
+              type="file"
+              id="file-upload"
+              className="absolute left-0 top-0 h-full w-full cursor-pointer opacity-0"
+            />
             <div className="flex h-[200px] w-[200px] items-center justify-center rounded-full border-4 border-gray-500 bg-white">
               <label htmlFor="file-upload" className="cursor-pointer">
                 <span className="text-8xl text-gray-500">
