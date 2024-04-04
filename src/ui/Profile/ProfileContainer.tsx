4760760031ef1da0761c0dbeb381a6a23011f91b
Noran Sherif <noranosherif958@gmail.com>
2024-04-04 17:47:13 +0200

    * Split profile into reusable components
    - Add profileInputFields component
    - Add UploadPhoto Component

--- src/ui/Profile/ProfileContainer.tsx
+++ src/ui/Profile/ProfileContainer.tsx
@@ -1,93 +1,27 @@
-import { IoCloudUploadOutline } from "react-icons/io5";
-import Button from "../Button";
 import { MdEdit } from "react-icons/md";
-import { useRef } from "react";
 import { IoIosArrowDown } from "react-icons/io";
+import UploadPhoto from "./UploadPhoto";
+import ProfileInputField from "./ProfileInputField";
 
 const ProfileContainer = () => {
-  const ref = useRef<HTMLInputElement>(null);
-
   return (
     <div className="mt-[100px] flex min-h-screen items-start justify-center">
       <div className="relative bg-gradient-to-br from-linearBlue-2 to-linearOrange-100 p-[4px] ">
         <div
           className="flex h-[450px] w-[1000px] content-between items-center justify-start gap-16 
-        bg-white px-[100px] tablet:h-[650px] tablet:w-[400px] tablet:flex-col 
-        tablet:justify-center tablet:gap-10 mobile:h-[550px] mobile:w-[330px] mobile:flex-col mobile:justify-center mobile:gap-8"
+        bg-white px-[100px] mobile:h-[550px] mobile:w-[330px] mobile:flex-col 
+        mobile:justify-center mobile:gap-8 tablet:h-[650px] tablet:w-[400px] tablet:flex-col tablet:justify-center tablet:gap-10"
         >
           {/* Profile Image */}
-          <div className="relative flex flex-col items-center gap-8">
-            <input
-              type="file"
-              id="file-upload"
-              className="hidden cursor-pointer"
-              ref={ref}
-            />
-            <div
-              className="flex h-[200px] w-[200px] items-center justify-center rounded-full border-4 border-gray-500
-             bg-white tablet:h-[150px] tablet:w-[150px] mobile:h-[120px] mobile:w-[120px] "
-            >
-              <span className="text-8xl text-gray-500 tablet:text-7xl mobile:text-6xl">
-                <IoCloudUploadOutline />
-              </span>
-            </div>
-
-            <Button
-              type="primary"
-              size="xl"
-              onClick={() => {
-                if (ref.current) {
-                  ref.current.click();
-                }
-              }}
-            >
-              Upload Photo
-            </Button>
-          </div>
+          <UploadPhoto />
 
           {/*input fields*/}
-          <div className=" grid grid-cols-2 grid-rows-2 gap-x-10 gap-y-16 tablet:flex tablet:flex-col tablet:gap-8 mobile:flex mobile:flex-col mobile:gap-6">
-            <div className=" w-fit bg-gradient-to-br from-linearBlue-2 to-linearOrange-100 pb-[1px]">
-              <div className="flex h-[35px] w-[250px] justify-between bg-white">
-                <input
-                  type="text"
-                  className="w-full px-2 text-lg text-gray-600 focus:outline-none disabled:bg-transparent"
-                  placeholder="Username"
-                  disabled={true}
-                />
-                <span className="text-2xl text-gray-500">
-                  <MdEdit />
-                </span>
-              </div>
-            </div>
+          <div className=" grid grid-cols-2 grid-rows-2 gap-x-10 gap-y-16 mobile:flex mobile:flex-col mobile:gap-6 tablet:flex tablet:flex-col tablet:gap-8">
+            <ProfileInputField type="text" placeholder="Username" />
 
-            <div className=" w-fit bg-gradient-to-br from-linearBlue-2 to-linearOrange-100 pb-[1px]">
-              <div className="flex h-[35px] w-[250px] justify-between bg-white">
-                <input
-                  type="text"
-                  className="w-full px-2 text-lg text-gray-600 focus:outline-none disabled:bg-transparent"
-                  placeholder="Password"
-                  disabled={true}
-                />
-                <span className="text-2xl text-gray-500">
-                  <MdEdit />
-                </span>
-              </div>
-            </div>
+            <ProfileInputField type="password" placeholder="Password" />
 
-            <div className=" w-fit bg-gradient-to-br from-linearBlue-2 to-linearOrange-100 pb-[1px]">
-              <div className="flex h-[35px] w-[250px] justify-between bg-white">
-                <input
-                  type="text"
-                  className="w-full px-2 text-lg text-gray-600 focus:outline-none disabled:bg-transparent"
-                  placeholder="Email"
-                  disabled={true}
-                />
-                <span className="text-2xl text-gray-500">
-                  <MdEdit />
-                </span>
-              </div>
-            </div>
+            <ProfileInputField type="email" placeholder="Email" />
 
             <div className=" w-fit bg-gradient-to-br from-linearBlue-2 to-linearOrange-100 pb-[1px]">
               <div className="flex h-[35px] w-[250px] justify-between bg-white ">
--- src/ui/Profile/ProfileInputField.tsx
+++ src/ui/Profile/ProfileInputField.tsx
@@ -1,5 +1,41 @@
-const ProfileInputField = () => {
-  return <div>ProfileInputField</div>;
+import { useEffect, useRef, useState } from "react";
+import { MdEdit } from "react-icons/md";
+
+interface Props {
+  type: string;
+  placeholder: string;
+}
+
+const ProfileInputField = ({ type, placeholder }: Props) => {
+  const [disabled, setDisabled] = useState<boolean>(true);
+  const inputField = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputField.current) {
+      inputField.current.focus();
+    }
+  }, [disabled]);
+
+  function handleClick() {
+    setDisabled((e) => !e);
+  }
+
+  return (
+    <div className=" w-fit bg-gradient-to-br from-linearBlue-2 to-linearOrange-100 pb-[1px]">
+      <div className="flex h-[35px] w-[250px] justify-between bg-white">
+        <input
+          type={type}
+          className="w-full px-2 text-lg text-gray-600 focus:outline-none disabled:bg-transparent"
+          placeholder={placeholder}
+          disabled={disabled}
+          ref={inputField}
+        />
+        <span className="text-2xl text-gray-500" onClick={handleClick}>
+          <MdEdit />
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default ProfileInputField;
