612056684b6943c12309e152f8b5a86209737328
AhmedZedan <116225212+1Ahmedzedan@users.noreply.github.com>
2024-04-15 18:14:02 +0200

    Update BlogInfo.test.tsx

--- src/__tests__/BlogInfo.test.tsx
+++ src/__tests__/BlogInfo.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { HashRouter } from "react-router-dom";
 import BlogInfo from "../ui/Blog/BlogDetails/BlogInfo";
 
