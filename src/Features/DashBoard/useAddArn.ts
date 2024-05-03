3b193e3b5a1de28a2cff962a0d1bb4c887b4144f
AhmedZedan <116225212+1Ahmedzedan@users.noreply.github.com>
2024-05-04 01:26:00 +0300

    Fetch Billing Data (#30)

--- src/Features/DashBoard/useAddArn.ts
+++ src/Features/DashBoard/useAddArn.ts
@@ -1,19 +1,21 @@
 import { useMutation } from "@tanstack/react-query";
-import { addARN} from "../../services/authenication";
+import { addARN } from "../../services/dashboard";
 import { handleToastMessage } from "../../utils/helper";
 import { useNavigate } from "react-router-dom";
-
-
 
 const useAddARN = () => {
 	const navigate = useNavigate();
 	const { mutate, isPending } = useMutation({
-		mutationFn: ({ ARN, token }: { ARN: string; token: string | undefined }) =>
-			addARN(ARN, token),
+		mutationFn: ({
+			ARN,
+			token,
+		}: {
+			ARN: string;
+			token: string | undefined;
+		}) => addARN(ARN, token),
 		onSuccess: () => {
 			handleToastMessage("ARN is successfully!", "success");
 			navigate("/dashboard/charts");
-
 		},
 		onError: (error) => {
 			handleToastMessage(error.message, "warning");
