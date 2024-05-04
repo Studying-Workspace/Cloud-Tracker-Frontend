89d216052c2df18bc28b6aa23e9f8d6234731c57
1Ahmedzedan <ugs.141182@ci.suez.edu.eg>
2024-05-04 20:43:47 +0300

    Add style to  filter date for charts data component

--- src/ui/DashBoard/FilterDate.tsx
+++ src/ui/DashBoard/FilterDate.tsx
@@ -2,35 +2,42 @@ import { SlCalender } from "react-icons/sl";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { useState } from "react";
+import useGetBillingData from "../../Features/DashBoard/useGetBillingData";
+import { defaultDates } from "../../utils/billingDataHandling";
 const FilterDate = () => {
-	const [startDate, setStartDate] = useState<Date | null>(
-		new Date(new Date()),
+	const { BillingData } = useGetBillingData();
+	const { startDate, endDate } = defaultDates(BillingData);
+
+	const [inputStartDate, setInputStartDate] = useState<Date | null>(
+		new Date(startDate),
 	);
-	// const ExampleCustomInput = forwardRef(({ value, onClick } : {value:any , onClick:Function}, ref) => (
-	// 	<button className="example-custom-input" onClick={onClick} ref={ref}>
-	// 		{value}
-	// 	</button>
-	// ));
+
+	const [inputEndDate, setInputEndDate] = useState<Date | null>(
+		new Date(endDate),
+	);
+
+	const [granularity , setGranularity] = useState<string>("m") ;
 
 	return (
-		<div className="flex items-center justify-between gap-10">
+		<div className="flex items-center justify-between gap-40">
 			<div className="flex items-center gap-6 text-linearOrange-200">
-				<button>Daily</button>
-				<button>Monthly</button>
+				<button onClick={()=>setGranularity("d")} className={`${granularity === "d" ? " font-bold text-lg" : ""}`}>Daily</button>
+				<button onClick={()=>setGranularity("m")} className={`${granularity === "m" ? " font-bold text-lg" : ""}`}>Monthly</button>
 			</div>
 			<div className="flex items-center gap-4">
 				<span className=" text-gray-400">
 					<SlCalender />
 				</span>
-				<DatePicker
-					selected={startDate}
-					onChange={(date) => setStartDate(date)}
-				/>
-				<span>-</span>
-				<DatePicker
-					selected={startDate}
-					onChange={(date) => setStartDate(date)}
-				/>
+				<div className="flex flex-col">
+					<DatePicker
+						selected={inputStartDate}
+						onChange={(date) => setInputStartDate(date)}
+					/>
+					<DatePicker
+						selected={inputEndDate}
+						onChange={(date) => setInputEndDate(date)}
+					/>
+				</div>
 			</div>
 		</div>
 	);
