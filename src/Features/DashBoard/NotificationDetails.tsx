f984cfdbe7583e939390536284ca90a375cf7573
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2024-06-30 18:38:18 +0300

    refactor NotficationDetails to add real RI data
    - rename NotificationDetail to NotificationDetails
    - refactor the component to receive RIList as a prop
    - replace mockData with the actual data passed through the props
    - render the duration in months instead of seconds

---
+++ src/Features/DashBoard/NotificationDetails.tsx
@@ -0,0 +1,28 @@
+import { NotificationDetailsProps } from "./notification-types";
+import { convertSecondsToMonths } from "../../utils/helper";
+
+const NotificationDetails = ({ RIList }: NotificationDetailsProps) => {
+  return (
+    <table className="w-full table-auto mobile:text-xs" data-testid="table">
+      <tbody>
+        <tr className="bg-linearBlue-1 font-semibold text-white">
+          <td className="py-2 text-center">Instance Type</td>
+          <td className="py-2 text-center">OS</td>
+          <td className="py-2 text-center">Price</td>
+          <td className="py-2 text-center">Duration</td>
+        </tr>
+
+        {RIList.map((RI) => (
+          <tr className="border border-linearBlue-1 font-semibold text-black">
+            <td className="py-2 text-center">{RI.instanceType}</td>
+            <td className="py-2 text-center">{RI.os}</td>
+            <td className="py-2 text-center">{RI.price}</td>
+            <td className="py-2 text-center">{convertSecondsToMonths(RI.duration)} months</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default NotificationDetails;
