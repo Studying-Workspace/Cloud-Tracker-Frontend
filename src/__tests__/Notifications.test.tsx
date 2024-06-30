f5d2bd1dd63249df08bfb3aeef8338c907081c7d
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2024-06-30 19:23:52 +0300

    add mock notificationItemProps
    - create a mock props data for the NotificationItem

--- src/__tests__/Notifications.test.tsx
+++ src/__tests__/Notifications.test.tsx
@@ -4,69 +4,84 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import UserProvider from "../context/UserProvider";
 import NotificationItem from "../Features/DashBoard/NotificationItem";
 import Notifications from "../pages/Notifications";
+import { NotificationItemProps } from "../Features/DashBoard/notification-types";
 
 const queryClient = new QueryClient({
-	defaultOptions: {
-		queries: {
-			staleTime: 0,
-		},
-	},
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
 });
 
 describe("Notifications page", () => {
-	beforeEach(() => {
-		render(
-			<HashRouter>
-				<UserProvider>
-					<QueryClientProvider client={queryClient}>
-						<Notifications />
-					</QueryClientProvider>
-				</UserProvider>
-			</HashRouter>,
-		);
-	});
+  beforeEach(() => {
+    render(
+      <HashRouter>
+        <UserProvider>
+          <QueryClientProvider client={queryClient}>
+            <Notifications />
+          </QueryClientProvider>
+        </UserProvider>
+      </HashRouter>,
+    );
+  });
 
-	it("should render correctly", () => {
-		expect(screen.getByTestId("NotificationPage")).toBeInTheDocument();
-	});
+  it("should render correctly", () => {
+    expect(screen.getByTestId("notification-container")).toBeInTheDocument();
+  });
 });
 
 describe("Notification Item", () => {
-	beforeEach(() => {
-		render(
-			<HashRouter>
-				<UserProvider>
-					<QueryClientProvider client={queryClient}>
-						<NotificationItem
-							instanceName="EC2"
-							priceOnDemand="$30"
-							priceReservedInstance="$50"
-						/>
-					</QueryClientProvider>
-				</UserProvider>
-			</HashRouter>,
-		);
-	});
+  beforeEach(() => {
+    const notificationItemProps: NotificationItemProps = {
+      instanceInfo: {
+        instance: {
+          instanceType: "test-used-instance",
+          region: "test-used-region",
+          os: "test-used-os",
+          cost: 10.0,
+        },
+        ridtos: [
+          {
+            instanceType: "test-ri-type",
+            os: "test-ri-os",
+            duration: 20,
+            price: 30.0,
+          },
+        ],
+      },
+    };
+    render(
+      <HashRouter>
+        <UserProvider>
+          <QueryClientProvider client={queryClient}>
+            <NotificationItem notificationItemProps={notificationItemProps} />
+          </QueryClientProvider>
+        </UserProvider>
+      </HashRouter>,
+    );
+  });
 
-	it("should have a description for notification item", () => {
-		expect(screen.getByTestId("description")).toBeInTheDocument();
-	});
+  it("should have a description for notification item", () => {
+    expect(screen.getByTestId("description")).toBeInTheDocument();
+  });
 
-	it("should have a title for notification item", () => {
-		expect(screen.getByTestId("title")).toBeInTheDocument();
-	});
+  it("should have a title for notification item", () => {
+    expect(screen.getByTestId("title")).toBeInTheDocument();
+  });
 
-	it("should have a button to show details", () => {
-		expect(screen.getByText("Show Options")).toBeInTheDocument();
-	});
+  it("should have a button to show details", () => {
+    expect(screen.getByText("Show Options")).toBeInTheDocument();
+  });
 
-	it ("should have an image", () => {
-		expect(screen.getByAltText("img")).toBeInTheDocument();
-	});
+  it("should have an image", () => {
+    expect(screen.getByAltText("img")).toBeInTheDocument();
+  });
 
-	it("should toggle a table on button click", () => {
-		const button = screen.getByText("Show Options");
-		fireEvent.click(button);
-		expect(screen.getByTestId("table")).toBeInTheDocument();
-	}	);
+  it("should toggle a table on button click", () => {
+    const button = screen.getByText("Show Options");
+    fireEvent.click(button);
+    expect(screen.getByTestId("table")).toBeInTheDocument();
+  });
 });
