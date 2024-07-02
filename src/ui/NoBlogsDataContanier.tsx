3dc54b62e4d91790dc8e3fea8f231f0bd29a2848
Shahd Elmeniawy <shahedmohamed180@gmail.com>
2024-07-02 07:40:10 +0300

    Fixed NotFound page gif

--- src/ui/NoBlogsDataContanier.tsx
+++ src/ui/NoBlogsDataContanier.tsx
@@ -5,7 +5,7 @@ const NoBlogsDataContanier = () => {
 		<div className="flex flex-col items-center space-y-3 mobile:space-y-7">
 			<img
 				alt="No Data"
-				className="w-[40%] object-cover mobile:mt-9 mobile:w-[85%] tablet:mt-32 tablet:w-[85%]"
+				className="w-[35%] object-cover mobile:mt-9 mobile:w-[75%] tablet:mt-32 tablet:w-[75%]"
 				src={NoData}
 			/>
 			<p className="text-2xl font-bold mobile:text-lg tablet:text-3xl">
--- src/ui/NotFound.tsx
+++ src/ui/NotFound.tsx
@@ -1,5 +1,6 @@
 import Button from "./Button";
 import Wave from "../assets/introWave.png";
+import notfoundimg from "../assets/notFoundPixels.gif";
 
 const NotFound = () => {
 	return (
@@ -15,7 +16,7 @@ const NotFound = () => {
 					alt="Not Found"
 					data-testid="not-found"
 					className="w-[40%] object-cover mobile:mt-16 mobile:w-[85%] tablet:mt-32 tablet:w-[70%]"
-					src="https://s3-alpha-sig.figma.com/img/8ca6/fe87/fc9975b15429f59f074ee1d34e419ff5?Expires=1715558400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=dfpenwo6QarmVqQLPT34PanktCkkJz7SqUN4lS4xrsfMMYP2o9WtsCNHkb~H7HNOO3Q07LalJk0O098xbKqB6t2quTDpEzZ026nMgIw5Ff9X-4M0Qqw4w1RO~WOVEBdrJ9~IZot-7jPaPrYSD~qlsxO6t6Gfmv-Fo1fzpRKE717gvAF3hHt4c54C5uvb9AnStpk1Y0hU95~pLSMMwGB6itxpOCgi6kJpLukzghicW-8OkJiwFevxRQLhxbaSW-wfniBpt-LJXuf6LLJRfpLUlOViVdEnZTcnYOF2ejTCctEMnieAUYaLMjK22KPZBU5rFFYbreoq1PudHBxxY7xZtA__"
+					src={notfoundimg}
 				/>
 				<Button size="2xl" onClick={() => (window.location.href = "/")}>
 					Go Home
